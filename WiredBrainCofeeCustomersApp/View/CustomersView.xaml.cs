db3527fd70ee3f58b2c86633a72671689a51986d
Ross O'Donegan <rossodonegan123@gmail.com>
2022-12-11 21:37:15 +0000

    Finshed "Organizing Code with UserControls" module

---
+++ WiredBrainCofeeCustomersApp/View/CustomersView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WiredBrainCofeeCustomersApp.View
+{
+    /// <summary>
+    /// Interaction logic for CustomersView.xaml
+    /// </summary>
+    public partial class CustomersView : UserControl
+    {
+        public CustomersView()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            //var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+
+            //var newColumn = column == 0 ? 2 : 0;
+
+            //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+            var column = Grid.GetColumn(customerListGrid);
+
+            var newColumn = column == 0 ? 2 : 0;
+
+            Grid.SetColumn(customerListGrid, newColumn);
+        }
+    }
+}
