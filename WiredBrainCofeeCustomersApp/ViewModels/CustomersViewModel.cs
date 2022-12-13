39ad41e56838c447c54c35aaffae9bbcbe9acbfa
Ross <ross.odonegan@assaabloy.com>
2022-12-13 16:55:16 +0000

    Started work on the MVVM module

---
+++ WiredBrainCofeeCustomersApp/ViewModels/CustomersViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WiredBrainCofeeCustomersApp.Models;
+
+namespace WiredBrainCofeeCustomersApp.ViewModels
+{
+    public class CustomersViewModel
+    {
+        public ObservableCollection<Customer> Customers { get; set; }
+
+    }
+}
